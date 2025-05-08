a2d1b53a1180e694366bdbc532a871ffd6e897b1
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-05-08 20:16:45 -0300

    operadores

--- app01.js
+++ app01.js
@@ -23,11 +23,34 @@ let fumante= false
 console.log(typeof cidade);
 console.log(typeof salario);
 console.log(typeof fumante);
+
 // Operadores
-//     Operador Atribuição
-//     Operadores aritméticos
-//     Operadores relacionais
-//     Operadores lógicos
+//     Operador Atribuição (=)
+
+//     Operadores aritméticos ( +, -, *, /, %)
+console.log(10 + 5);
+console.log(10 - 5);
+let n1 = 10;   
+let n2 = 5;
+console.log(n1 + n2); //soma
+console.log(n1 * n2); //multiplicação
+console.log(n1 / n2); //divisão
+console.log(n1 - n2); //subtração
+console.log(n1 % 3); //resto da divisão
+
+//     Operadores relacionais (==, !=, >, <, >=, <=)
+console.log(n1 == n2); //igualdade
+console.log(n1 != n2); //diferença
+console.log(n1 > n2); //maior
+console.log(n1 < n2); //menor
+console.log(n1 >= n2); //maior ou igual
+console.log(n1 <= n2); //menor ou igual
+
+//     Operadores lógicos (&&, ||, !)
+console.log(!10 > 2); //negação
+console.log(!false); //negação
+console.log(10 > 2 && 5 > 3 && 2 < 1); //e - Todas as expressões devem ser verdadeiras para retornar true
+console.log(10 > 2 || 5 > 3 || 2 < 1); //ou - Apenas uma expressão precisa ser verdadeira para retornar true
 
 // Estrutura
 //     Estrutura de controle/decisão
